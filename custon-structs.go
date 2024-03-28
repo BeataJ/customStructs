5bb4b711a010d1e3ca22da212fc0384990fb540d
Beata <beata@jasinski.com>
2024-03-27 22:09:42 -0700

    custom type

--- custon-structs.go
+++ custon-structs.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "fmt"
+
+type customString string
+
+func (text customString) log() {
+	fmt.Println(text)
+}
+
+func main() {
+	var name customString = "max"
+
+	name.log()
+}
